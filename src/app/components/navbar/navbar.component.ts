95d99093a2fd6c89227438d08bc6477aa18352e4
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-09-03 23:53:11 -0500

    debugging and functional testing

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginServicesService } from '../../services/login-services.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  constructor(public login: LoginServicesService) { }
+
+  ngOnInit(): void {
+  }
+
+  public logout() {
+    this.login.logout();
+    window.location.reload();
+  }
 
 }
